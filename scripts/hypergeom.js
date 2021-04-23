1ce3f4c2df8a8f4d16d056f13360db78e493a913
zmuranaka <zmuranaka@gmail.com>
2021-04-23 16:36:51 -0600

    Large overhaul of hypergeom.js

--- scripts/hypergeom.js
+++ scripts/hypergeom.js
@@ -6,14 +6,20 @@ Zachary Muranaka
 Solves a hypergeometric distribution
 */
 
+// Array that contains the input boxes from the document
+const inputBoxes = document.querySelectorAll('input[type="number"]');
+
+// Array that contains the results list items from the document
+const resultsList = document.getElementById("results").getElementsByTagName("li");
+
 // Calculates the greatest common denominator between two numbers
 function gcd(givenNumber1, givenNumber2)
 {
     var greatestCommonDenominator = 1; // Any two numbers' minimum gcd is 1
 
-    for(let i = 1; i <= givenNumber1 && i <= givenNumber2; ++i)
+    for (let i = 1; i <= givenNumber1 && i <= givenNumber2; ++i)
     {
-        if(givenNumber1 % i == 0 && givenNumber2 % i == 0) // Checks if i is factor of both integers using modulus division
+        if (givenNumber1 % i == 0 && givenNumber2 % i == 0) // Checks if i is factor of both integers using modulus division
             greatestCommonDenominator = i;
     }
 
@@ -33,9 +39,9 @@ function ncr(n, r)
      * The combination of n and r is equal to the combination of n and (n - r)
      * For example, 17 combination 13 is the same as 17 combination 4
      * Therefore, we can substitute (n - r) for r if we have a larger r value
-     * Attempting to prevent overflow, if(n - r) is less than r we use it instead
+     * Attempting to prevent overflow, if (n - r) is less than r we use it instead
      */
-    if(n - r < r) r = n - r;
+    if (n - r < r) r = n - r;
 
     // Tries to calculate the combination of n and r
     try
@@ -43,7 +49,7 @@ function ncr(n, r)
         var top = 1; // top holds the value of n * (n - 1) * (n - 2) ...
         var bottom = 1; // bottom holds the value of r * (r - 1) * (r - 2) ...
 
-        if(r != 0)
+        if (r)
         {
             /*
              * Because this only loops until r = 0, we only calculate the first r numbers of the factorial
@@ -52,7 +58,7 @@ function ncr(n, r)
              * This is how we calculate nCr with the simplified equation
              * The top is n * (n - 1) * (n - 2) * ... * (n - r + 1) and the bottom is r!
              */
-            while(r > 0)
+            while (r)
             {
                 top *= n;
                 bottom *= r;
@@ -67,8 +73,7 @@ function ncr(n, r)
                 r--;
             }
         }
-        else // n combination 0, where n is any number, is always equal to 1
-            top = 1;
+        else top = 1; // n combination 0, where n is any number, is always equal to 1
 
         /*
          * A factorial always simplifies to a whole number
@@ -84,28 +89,6 @@ function ncr(n, r)
     }
 }
 
-// Calculates the hypergeometric probability
-function probability(popSize, popSuccesses, desiredSuccesses, popFailures, sampleFailures)
-{
-    var w = 1;
-
-    for(let i = 0; i < desiredSuccesses; i++)
-    {
-        w = (popSuccesses / popSize) * w;
-        popSuccesses--;
-        popSize--;
-    }
-
-    for(let i = 0; i < sampleFailures; i++)
-    {
-        w = (popFailures / popSize) * w;
-        popFailures--;
-        popSize--;
-    }
-
-    return w;
-}
-
 // Class specification for a Deck object
 class Deck
 {
@@ -113,8 +96,9 @@ class Deck
     constructor(popSize = 0, popSuccesses = 0, sampleSize = 0, desiredSuccesses = 0)
     {
         // Checks if the numbers they entered allow for a valid Deck
-        if(popSize >= popSuccesses && popSize >= sampleSize && popSize >= desiredSuccesses && popSuccesses >= desiredSuccesses &&
-        sampleSize >= desiredSuccesses && popSize >= 0 && popSuccesses >= 0 && sampleSize >= 0 && desiredSuccesses >= 0)
+        if (popSize >= popSuccesses && popSize >= sampleSize && popSize >= desiredSuccesses
+            && popSuccesses >= desiredSuccesses && sampleSize >= desiredSuccesses
+            && popSize >= 0 && popSuccesses >= 0 && sampleSize >= 0 && desiredSuccesses >= 0)
         {
             try
             {
@@ -122,14 +106,17 @@ class Deck
                 this.popSuccesses = popSuccesses; // The number of successes within the population
                 this.sampleSize = sampleSize; // The size of the sample you take from the entire population
                 this.desiredSuccesses = desiredSuccesses; // The amount of successes you are looking for from your sample
+                this.popFailures = popSize - popSuccesses;
 
-                this.popFailures = popSize - popSuccesses; // Calculates the amount of failures throughout the entire population
-                this.sampleFailures = sampleSize - desiredSuccesses; // Calculates the number of failures in your sample assuming you get exactly the desired number of successes
-
-                this.combination = ncr(sampleSize, desiredSuccesses); // The combination of sampleSize and desiredSuccesses (nCr, where n = sampleSize and r = desiredSuccesses)
+                this.exactChance = this.probability(this.desiredSuccesses);
+                this.orGreaterInclusiveChance = this.orGreater(this.exactChance);
+                this.orLessInclusiveChance = this.orLess(this.exactChance);
             }
             // It is possible to construct a valid Deck that still results in a divide-by-zero error
-            catch(err) { this.constructorFailure(); }
+            catch(err)
+            {
+                this.constructorFailure();
+            }
         }
         else this.constructorFailure();
     }
@@ -143,21 +130,53 @@ class Deck
         this.sampleSize = 0;
         this.desiredSuccesses = 0;
         this.popFailures = 0;
-        this.sampleFailures = 0;
-        this.combination = 0;
+        this.exactChance = 0;
+        this.orGreaterInclusiveChance = 0;
+        this.orLessInclusiveChance = 0;
+    }
+
+    // Calculates the hypergeometric probability
+    probability(currentDesiredSuccesses)
+    {
+        // Local variables are used so the member variables of the class are not changed
+        var localPopSize = this.popSize;
+        var localPopSuccesses = this.popSuccesses;
+        var localPopFailures = this.popFailures;
+        var sampleFailures = this.sampleSize - currentDesiredSuccesses;
+
+        var prob = 1;
+        var combination = ncr(this.sampleSize, currentDesiredSuccesses);
+
+        // Calculate the probability from the successes
+        while (currentDesiredSuccesses > 0)
+        {
+            prob *= (localPopSuccesses / localPopSize);
+            localPopSuccesses--;
+            localPopSize--;
+            currentDesiredSuccesses--;
+        }
+
+        // Calculate the probability from the failures
+        while (sampleFailures > 0)
+        {
+            prob *= (localPopFailures / localPopSize);
+            localPopFailures--;
+            localPopSize--;
+            sampleFailures--;
+        }
+
+        return prob * combination;
     }
 
     // Calculates the probability of n or greater successes
     orGreater(exactChance)
     {
-        for(let i = this.desiredSuccesses; i < this.sampleSize; i++)
+        var tempDesiredSuccesses = this.desiredSuccesses;
+
+        while (tempDesiredSuccesses < this.sampleSize)
         {
-            this.desiredSuccesses++;
-            this.combination = ncr(this.sampleSize, this.desiredSuccesses);
-            this.sampleFailures = this.sampleSize - this.desiredSuccesses;
-            var iChance = probability(this.popSize, this.popSuccesses, this.desiredSuccesses, this.popFailures, this.sampleFailures);
-            iChance = iChance * this.combination;
-            exactChance += iChance;
+            tempDesiredSuccesses++;
+            exactChance += this.probability(tempDesiredSuccesses);
         }
 
         return exactChance;
@@ -166,130 +185,76 @@ class Deck
     // Calculates the probability of n or less successes
     orLess(exactChance)
     {
-        for(let i = this.desiredSuccesses; i > 0; i--)
+        var tempDesiredSuccesses = this.desiredSuccesses;
+
+        while (tempDesiredSuccesses > 0)
         {
-            this.desiredSuccesses--;
-            this.combination = ncr(this.sampleSize, this.desiredSuccesses);
-            this.sampleFailures = this.sampleSize - this.desiredSuccesses;
-            var iChance = probability(this.popSize, this.popSuccesses, this.desiredSuccesses, this.popFailures, this.sampleFailures);
-            iChance = iChance * this.combination;
-            exactChance += iChance;
+            tempDesiredSuccesses--;
+            exactChance += this.probability(tempDesiredSuccesses);
         }
         
         return exactChance;
     }
 
     // The following six methods are getters for class variables
-    get sizeOfPopulation(){ return this.popSize; }
-
-    get successesInPopulation(){ return this.popSuccesses; }
-
-    get successesInSample(){ return this.desiredSuccesses; }
-
-    get failuresInPopulation(){ return this.popFailures; }
-
-    get failuresInSample(){ return this.sampleFailures; }
-
-    get sampleCombinations(){ return this.combination; }
+    get exact() { return this.exactChance; }
+    get orGreaterInclusive() { return this.orGreaterInclusiveChance; }
+    get orLessInclusive() { return this.orLessInclusiveChance; }
 }
 
 document.getElementById("calculateBtn").addEventListener("click", calculate); // Clicking the calculatebtn calls the calculate() function
 
 // Add an event listner to every number input that calls the calculate() function when their value changes
-var numberInputs = document.querySelectorAll('input[type="number"]');
-for(let i = 0; i < numberInputs.length; i++)
-    numberInputs[i].addEventListener("change", calculate);
+for (let i = 0; i < inputBoxes.length; i++)
+    inputBoxes[i].addEventListener("change", calculate);
 
 
 // Add an event listener to the page so pressing enter calls the calculate() function
 window.addEventListener("keyup",
 function(e)
 {
-    if(e.keyCode == 13) // 13 is the keycode for enter
-        calculate();
+    if (e.key == "Enter") calculate();
 });
 
 
-// Calculates the hypergeometric probability
+// Calculates the hypergeometric probability and dynamically writes the results to the HTML
 function calculate()
 {
-    var popSizeInput, popSuccessesInput, sampleSizeInput, desiredSuccessesInput;
+    var userInputs = [0, 0, 0, 0]; // Array that contains the user's input from the document
+
     /* 
      * Sets the local variables to be equal to whatever the user entered in the input boxes.
      * The || 0 means that if the parseInt() does not return a valid number, then the
      * variable is automatically set to 0. For example, if the user entered a number with multiple
      * decimal points, then instead of evaluating it as NaN it would just set it to 0.
      */
-    if(document.getElementById("popSize").value >= 0) // If the popSize input is at least 0
+    for (let i = 0; i < inputBoxes.length; i++)
     {
-        popSizeInput = parseInt(document.getElementById("popSize").value) || 0;
-        document.getElementById("popSize").value = popSizeInput;
-    }
-    else // The user entered a negative number
-    {
-        popSizeInput = 0;
-        document.getElementById("popSize").value = 0;
-    }
-
-    if(document.getElementById("popSuccesses").value >= 0) // If the popSuccesses input is at least 0
-    {
-        popSuccessesInput = parseInt(document.getElementById("popSuccesses").value) || 0;
-        document.getElementById("popSuccesses").value = popSuccessesInput;
-    }
-    else // The user entered a negative number
-    {
-        popSuccessesInput = 0;
-        document.getElementById("popSuccesses").value = 0;
+        if (inputBoxes[i].value >= 0) // If the input is at least 0
+        {
+            userInputs[i] = parseInt(inputBoxes[i].value) || 0;
+            inputBoxes[i].value = userInputs[i];
+        }
+        else // The user entered a negative number
+        {
+            userInputs[i] = 0;
+            inputBoxes[i].value = 0;
+        }
     }
 
-    if(document.getElementById("sampleSize").value >= 0) // If the sampleSize input is at least 0
-    {
-        sampleSizeInput = parseInt(document.getElementById("sampleSize").value) || 0;
-        document.getElementById("sampleSize").value = sampleSizeInput;
-    }
-    else // The user entered a negative number
-    {
-        sampleSizeInput = 0;
-        document.getElementById("sampleSize").value = 0;
-    }
-
-    if(document.getElementById("desiredSuccesses").value >= 0) // If the desiredSuccesses input is at least 0
-    {
-        desiredSuccessesInput = parseInt(document.getElementById("desiredSuccesses").value) || 0;
-        document.getElementById("desiredSuccesses").value = desiredSuccessesInput;
-    }
-    else // The user entered a negative number
-    {
-        desiredSuccessesInput = 0;
-        document.getElementById("desiredSuccesses").value = 0;
-    }
-
-    // Construct two identical deck objects
-    var deck = new Deck(popSizeInput, popSuccessesInput, sampleSizeInput, desiredSuccessesInput);
-    var deck2 = new Deck(popSizeInput, popSuccessesInput, sampleSizeInput, desiredSuccessesInput);
-    
-    // Calculate the probabilities
-    var exactChance = (probability(deck.sizeOfPopulation, deck.successesInPopulation, deck.successesInSample, deck.failuresInPopulation, deck.failuresInSample) * deck.sampleCombinations);
-    var orGreaterChance = deck.orGreater(exactChance);
-    var orLessChance = deck2.orLess(exactChance); // We use deck2 because the orGreaterChance method altered deck
+    // Construct a Deck object from the user's input
+    var deck = new Deck(userInputs[0], userInputs[1], userInputs[2], userInputs[3]);
 
     /*
-     * Write the HTML for the results
+     * Write the results to the HTML
      *
      * The toFixed(18) rounds the numbers to 18 decimal places and converts them to strings
      * The parseFloat() parses the strings back to floats which drops any excess zeroes
      * For example 0.toFixed(18) would be 0.000000000000000000, but parseFloat(0.toFixed(18)) is just 0
      */
-    var exactlyHTML = "Chance of exactly desired successes: " + "<b>" + parseFloat(exactChance.toFixed(18)) + "</b>";
-    var lessThanHTML = "Chance of less than desired successes: " + "<b>" + parseFloat((orLessChance - exactChance).toFixed(18)) + "</b>";
-    var orLessHTML = "Chance of desired successes or less: " + "<b>" + parseFloat(orLessChance.toFixed(18)) + "</b>";
-    var greaterThanHTML = "Chance of greater than desired successes: " + "<b>" + parseFloat((orGreaterChance - exactChance).toFixed(18)) + "</b>";
-    var orGreaterHTML = "Chance of desired successes or greater: " + "<b>" + parseFloat(orGreaterChance.toFixed(18)) + "</b>";
-
-    // Display the results
-    document.getElementById("exactly").innerHTML = exactlyHTML;
-    document.getElementById("lessThan").innerHTML = lessThanHTML;
-    document.getElementById("orLess").innerHTML = orLessHTML;
-    document.getElementById("greaterThan").innerHTML = greaterThanHTML;
-    document.getElementById("orGreater").innerHTML = orGreaterHTML;
+    resultsList[0].innerHTML = "Chance of exactly desired successes: " + "<b>" + parseFloat(deck.exact.toFixed(18)) + "</b>";
+    resultsList[1].innerHTML = "Chance of less than desired successes: " + "<b>" + parseFloat((deck.orLessInclusive - deck.exact).toFixed(18)) + "</b>";
+    resultsList[2].innerHTML = "Chance of desired successes or less: " + "<b>" + parseFloat(deck.orLessInclusive.toFixed(18)) + "</b>";
+    resultsList[3].innerHTML = "Chance of greater than desired successes: " + "<b>" + parseFloat((deck.orGreaterInclusive - deck.exact).toFixed(18)) + "</b>";
+    resultsList[4].innerHTML = "Chance of desired successes or greater: " + "<b>" + parseFloat(deck.orGreaterInclusive.toFixed(18)) + "</b>";
 }
